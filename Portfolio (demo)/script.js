98c60346d04e9fed3720753ffbab3cf90e82701b
dybbuk210 <dybbuk210@gmail.com>
2024-12-02 15:22:04 +0100

    Aktualizácia kódu na git dorábam a pracujem na portfóliu

---
+++ Portfolio (demo)/script.js
@@ -0,0 +1,44 @@
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* scroll sections active link */
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__list a[href*="' + sectionId + '"]'); // pridanie správnych úvodzoviek
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
+/* scroll sections active link */
+
+/* Scroll animation */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true, // Animations repeat
+})
+
+sr.reveal('.perfil')
+sr.reveal('.info', {origin: 'left', delay: 800})
+sr.reveal('.skills', {origin: 'left', delay: 1000})
+sr.reveal('.about', {origin: 'right', delay:1200})
+sr.reveal('.projects__card, .services__card, .experience__card', {interval: 100})
